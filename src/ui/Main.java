784be651c81c832d259bd4e0758076653ff8c0fb
Nicolas Rios <nicolasq2929@gmail.com>
2021-05-22 07:02:35 -0500

    Solving some bugs

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,9 +6,7 @@ public class Main {
     private static Flag col;
     public static void main(String[] args) throws Exception {
 
-
-        col = new Flag(20, 37, 42, 5, 3, 3, 100);
-
+        col = new Flag(20, 37, 42, 5, 3, 3, 50);
         col.paintFlag();
         col.goFinal();
         
